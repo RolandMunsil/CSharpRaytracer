b7490b5c0f860cb0bf221db13e95348c0c5a8834
Roland Munsil <roland@munsil.com>
2015-09-07 19:23:26 -0500

    Added skeleton Torus and YCylinder classes

---
+++ Raytracer/Renderables/Torus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raytracer.Renderables
+{
+    class Torus : Renderable
+    {
+        Point3D position;
+
+        double torusRadius;
+        double tubeRadius;
+
+        public override Renderable.Intersection GetNearestIntersection(Ray ray)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Renderable.Intersection[] GetAllIntersections(Ray ray)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Contains(Point3D point)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Raytracer/Renderables/YCylinder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raytracer.Renderables
+{
+    class YCylinder : Renderable
+    {
+        double x;
+        double z;
+        double radius;
+
+        double yTop;
+        double yBottom;
+
+        public override Renderable.Intersection GetNearestIntersection(Ray ray)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Renderable.Intersection[] GetAllIntersections(Ray ray)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Contains(Point3D point)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
